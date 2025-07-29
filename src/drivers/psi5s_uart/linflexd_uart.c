bcee3a879135bc22dd5cb626d7dabbe460c8f39d
Daniel Oliveira <drawnpoetry@gmail.com>
2025-07-29 15:17:13 +0100

    ref(s32z270): cleanup uart driver
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/drivers/psi5s_uart/linflexd_uart.c
+++ src/drivers/psi5s_uart/linflexd_uart.c
@@ -1,415 +1,89 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
 
 #include <linflexd_uart.h>
 
-#define NULL ((void *)0)
-/* Array of pointers to UART driver runtime state structures */
-//static linflexd_uart_state_t * s_uartLINFlexDStatePtr[LINFlexD_INSTANCE_COUNT] = {NULL};
-
-
-uint32_t CGM0_GetAC8_SelValue(const MC_CGM_0_Type * base)
+void linflexd_uart_enable(volatile struct linflexd * uart)
 {
-    return ((base->MUX[8].CSS & MC_CGM_0_CSS_SELSTAT_MASK) >> MC_CGM_0_CSS_SELSTAT_SHIFT);
+    /* Request normal mode */
+    uart->lincr1 &= ~(LINFLEXD_LINCR1_SLEEP | LINFLEXD_LINCR1_INIT);
 }
 
-
-/*!
- * @brief Sets the word length.
- *
- * This function sets the word length in UART mode.
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param length UART mode word length.
- */
-void LINFLEXD_SetUartWordLength(LINFlexD_Type *base, linflexd_uart_word_length_t length)
+static void uart_set_baudrate(volatile struct linflexd* uart)
 {
-    uint32_t regValTemp;
+    uint32_t ibr;
 
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_WL0_MASK);
-    regValTemp |= LINFlexD_UARTCR_WL0((uint32_t)length & 1U);
-    regValTemp &= ~(LINFlexD_UARTCR_WL1_MASK);
-    regValTemp |= LINFlexD_UARTCR_WL1(((uint32_t)length >> 1U) & 1U);
-    base->UARTCR = regValTemp;
+    /* Compute the value for the integer baudrate */
+    ibr = LINFLEXD_0_CLKFREQ / (UART_BAUDRATE * LINFLEXD_0_DFLT_OSR);
+
+    /* Write the computed ibr */
+    uart->linibrr = ibr;
 }
 
-
-/*!
- * @brief Sets the transmission data field length/FIFO depth.
- *
- * This function sets either the transmission data field length or the number of bytes in the Tx FIFO, according to
- * the current transmission mode.
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param length data field length/FIFO depth.
- */
-void LINFLEXD_SetTxDataFieldLength(LINFlexD_Type *base, uint8_t length)
+void linflexd_uart_init(volatile struct linflexd * uart)
 {
-    uint32_t regValTemp;
+    /* Request init mode */
+    uart->lincr1 = (uart->lincr1 & ~(LINFLEXD_LINCR1_SLEEP)) | LINFLEXD_LINCR1_INIT;
 
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_TDFL_TFC_MASK);
-    regValTemp |= LINFlexD_UARTCR_TDFL_TFC(length);
-    base->UARTCR = regValTemp;
-}
+    /* Setup UART mode */
+    uart->uartcr |= (LINFLEXD_UARTCR_UART);
 
-
-/*!
- * @brief Sets the reception data field length/FIFO depth.
- *
- * This function sets either the reception data field length or the number of bytes in the Rx FIFO, according to
- * the current reception mode.
- *
- * @param base LINFlexD base pointer.
- * @param length data field length/FIFO depth.
- */
-void LINFLEXD_SetRxDataFieldLength(LINFlexD_Type *base, uint8_t length)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_RDFL_RFC_MASK);
-    regValTemp |= LINFlexD_UARTCR_RDFL_RFC(length);
-    base->UARTCR = regValTemp;
-}
-
-
-/*!
- * @brief Enables/disables parity bit.
- *
- * This function enables or disables the parity bit from UART bytes.
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param enable true - enable parity queuing/checking, false - disable parity queuing/checking.
- */
-void LINFLEXD_SetParityControl(LINFlexD_Type *base, bool enable)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_PCE_MASK);
-    regValTemp |= LINFlexD_UARTCR_PCE(enable ? 1UL : 0UL);
-    base->UARTCR = regValTemp;
-}
-
-/*!
- * @brief Sets the number of stop bits for Rx.
- *
- * This function sets the number of stop bits for received bytes.
- *
- * @param base LINFlexD base pointer.
- * @param stopBitsCount number of stop bits.
- */
-void LINFLEXD_SetRxStopBitsCount(LINFlexD_Type *base, linflexd_uart_stop_bits_count_t stopBitsCount)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_SBUR_MASK);
-    regValTemp |= LINFlexD_UARTCR_SBUR(stopBitsCount);
-    base->UARTCR = regValTemp;
-}
-
-
-/*!
- * @brief Sets the number of stop bits for Tx.
- *
- * This function sets the number of stop bits for transmitted bytes.
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param defaultOffset - true - use the memory map defined in the header file to access the register;
- *                      - false - use custom memory map to access the register.
- * @param stopBitsCount number of stop bits.
- */
-void LINFLEXD_SetTxStopBitsCount(LINFlexD_Type *base, linflexd_uart_stop_bits_count_t stopBitsCount,
-                                               bool defaultOffset)
-{
-    uint32_t regValTemp;
-    (void)defaultOffset;
-    regValTemp = base->GCR;
-    regValTemp &= ~(LINFlexD_GCR_STOP_MASK);
-    regValTemp |= LINFlexD_GCR_STOP(stopBitsCount);
-    base->GCR = regValTemp;
-}
-
-
-/*!
- * @brief Sets the transmission mode (FIFO/Buffer).
- *
- * This function sets the transmission mode (FIFO/Buffer).
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param mode tx mode: FIFO/Buffer.
- */
-void LINFLEXD_SetTxMode(LINFlexD_Type *base, linflexd_uart_rxtx_mode_t mode)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_TFBM_MASK);
-    regValTemp |= LINFlexD_UARTCR_TFBM((uint32_t)mode);
-    base->UARTCR = regValTemp;
-}
-
-/*!
- * @brief Sets the reception mode (FIFO/Buffer).
- *
- * This function sets the reception mode (FIFO/Buffer).
- * Should be called only when the module is in init mode.
- *
- * @param base LINFlexD base pointer.
- * @param mode rx mode: FIFO/Buffer.
- */
-void LINFLEXD_SetRxMode(LINFlexD_Type *base, linflexd_uart_rxtx_mode_t mode)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_RFBM_MASK);
-    regValTemp |= LINFlexD_UARTCR_RFBM((uint32_t)mode);
-    base->UARTCR = regValTemp;
-}
-
-void LINFLEXD_SetRxEnable(LINFlexD_Type *base, uint32_t enable)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp |= LINFlexD_UARTCR_RxEn(enable);
-    base->UARTCR = regValTemp;
-
-    regValTemp = base->LINIER;
-    regValTemp = (uint32_t) LINIER_DRIE;    
-    base->LINIER = regValTemp;
-}
-
-/*!
- * @brief Enables/disables an UART interrupt.
- *
- * This function configures whether the UART event will trigger an interrupt.
- *
- * @param base LINFlexD base pointer.
- * @param intSrc UART interrupt source.
- * @param enable true - enable interrupt, false - disable interrupt.
- */
-void LINFLEXD_SetInterruptMode(LINFlexD_Type * base, uint32_t/*linflexd_interrupt_t*/ intSrc, bool enable)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->LINIER;
-    if (enable)
-    {
-        regValTemp |= (uint32_t)intSrc;
-    }
-    else
-    {
-        regValTemp &= ~(uint32_t)intSrc;
-    }
-    base->LINIER = regValTemp;
-}
-
-
-
-/*!
- * @brief Enables/disables transmitter.
- *
- * This function enables or disables the UART transmitter, based on the parameter received.
- * Should be called only when the module is in UART mode.
- *
- * @param base LINFlexD base pointer.
- * @param enable enable/disable transmitter.
- */
-void LINFLEXD_SetTransmitterState(LINFlexD_Type *base, bool enable)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->UARTCR;
-    regValTemp &= ~(LINFlexD_UARTCR_TxEn_MASK);
-    regValTemp |= LINFlexD_UARTCR_TxEn(enable ? 1UL : 0UL);
-    base->UARTCR = regValTemp;
-}
-
-/*!
- * @brief Clears an UART interrupt flag.
- *
- * This function clears the UART status flag received as parameter.
- *
- * @param base LINFlexD base pointer.
- * @param flag UART status flag.
- */
-void LINFLEXD_ClearStatusFlag(LINFlexD_Type * base, linflexd_uart_status_flag_t flag)
-{
-    base->UARTSR = flag;
-}
-
-
-
-/*!
- * @brief Sets the first byte of the tx data buffer.
- *
- * This function writes one byte to the tx data buffer.
- *
- * @param base LINFlexD base pointer.
- * @param data data byte.
- */
-void LINFLEXD_SetTxDataBuffer1Byte(LINFlexD_Type * base, uint8_t data)
-{
-    uint32_t regValTemp;
-
-    regValTemp = base->BDRL;
-    regValTemp &= ~(LINFlexD_BDRL_DATA0_MASK);
-    regValTemp |= LINFlexD_BDRL_DATA0(data);
-    base->BDRL = regValTemp;
-}
-
-
-static inline uint8_t LINFLEXD_GetLinState(const LINFlexD_Type * base)
-{
-    uint8_t state = (uint8_t)((base->LINSR & LINFlexD_LINSR_LINS_MASK) >> LINFlexD_LINSR_LINS_SHIFT);
-    return state;
-}
-
-void LINFLEXD_UART_DRV_PutData(uint32_t instance, const uint8_t * txBuff)
-{
-    LINFlexD_Type * base;
-
-    base = s_LINFlexDBase[instance];
-
-    /* Wait for transmission to end */
-    while (LINFLEXD_GetLinState(base) == 7 || LINFLEXD_GetLinState(base) == 8);
-    LINFLEXD_SetTxDataBuffer1Byte(base, *txBuff);
-
-}
-
-/*!
- * @brief Gets the first byte of the rx data buffer.
- *
- * This function retrieves one byte from the rx data buffer.
- *
- * @param base LINFlexD base pointer.
- * @return data byte.
- */
-uint8_t LINFLEXD_GetRxDataBuffer1Byte(LINFlexD_Type * base)
-{
-    return (uint8_t)(base->BDRM & LINFlexD_BDRM_DATA4_MASK);
-}
-
-/*FUNCTION**********************************************************************
- *
- * Function Name : LINFLEXD_UART_DRV_StartSendUsingInterrupts
- * Description   : Initiate (start) a transmit by beginning the process of
- * sending data and enabling the interrupt.
- * This is not a public API as it is called from other driver functions.
- *
- *END**************************************************************************/
-uint32_t/*status_t*/ LINFLEXD_UART_DRV_StartSendUsingInterrupts(uint32_t instance,
-                                                           const uint8_t * txBuff,
-                                                           uint32_t txSize)
-{
-    LINFlexD_Type * base;
-
-    base = s_LINFlexDBase[instance];
-
-    /* Enable the transmitter */
-    LINFLEXD_SetTransmitterState(base, true);
-
-    /* Clear the tx empty flag to make sure the transmission of the first byte
-     * doesn't occur right after enabling the tx interrupt
+    /* Setup uart with the following configuration
+     * word length -> 8 Bits
+     * no parity
+     * buffer mode
+     * 115200
+     * Tx and Rx mode
      */
-    LINFLEXD_ClearStatusFlag(base, LINFlexD_UART_DATA_TRANSMITTED_FLAG);
+    uart->uartcr |= LINFLEXD_UARTCR_WL0 | LINFLEXD_UARTCR_TXEN | LINFLEXD_UARTCR_RXEN;
 
-    /* Enable transmission complete interrupt */
-    //LINFLEXD_SetInterruptMode(base, 1 /*LINFlexD_DATA_TRANSMITTED_INT*/, true);
-
-    /* Transmit the first word */
-    LINFLEXD_UART_DRV_PutData(instance, txBuff);
-
-    return 0;//STATUS_SUCCESS;
+    /* Set the baud rate */
+    uart_set_baudrate(uart);
 }
 
-
-
-/*FUNCTION**********************************************************************
- *
- * Function Name : LINFLEXD_UART_DRV_SendData
- * Description   : This function sends data using LINFlexD module in UART mode
- * with non-blocking method.
- *
- * Implements    : LINFLEXD_UART_DRV_SendData_Activity
- *END**************************************************************************/
-uint32_t/*status_t*/ LINFLEXD_UART_DRV_SendData(uint32_t instance,
-                                    const uint8_t * txBuff,
-                                    uint32_t txSize)
+void linflexd_uart_rxirq(volatile struct linflexd * uart)
 {
-    uint32_t/*status_t*/ retVal = 0;
-    retVal = LINFLEXD_UART_DRV_StartSendUsingInterrupts(instance, txBuff, txSize);
-    return retVal;
+    /* Enable data transmitted interrupt */
+    uart->linier |= LINFLEXD_LINIER_DRIE;
 }
 
-
-#define SIUL2_NUM_OF_PIN_PORT           16u
-
-
-void PINS_WritePin(GPIO_Type * const base,
-                                 pins_channel_type_t pin,
-                                 pins_level_type_t value)
+void linflexd_uart_clear_rxirq(volatile struct linflexd * uart)
 {
-    pins_channel_type_t pinsValues = (pins_channel_type_t)base->PGPDO;
-    pinsValues &= (pins_channel_type_t)(~((pins_channel_type_t)1U << (15U - pin)));
-    pinsValues |= (pins_channel_type_t)(((pins_channel_type_t)value & 1U) << (15U - pin));
-    base->PGPDO = pinsValues;
+    /* Clear the receive buffer full flag */
+    uart->uartsr |= LINFLEXD_UARTSR_DRFRFE;
 }
 
-void PINS_Init(const pin_settings_config_t * config)
+uint8_t linflexd_uart_getc(volatile struct linflexd * uart){
+
+    uint8_t data = 0;
+
+    while(!(uart->uartsr & ~LINFLEXD_UARTSR_RMB));
+
+    data = uart->bdrm & (0xFF);
+
+    linflexd_uart_clear_rxirq(uart);
+
+    return data;
+}
+
+void linflexd_uart_putc(volatile struct linflexd * uart, int8_t c)
 {
-    uint32_t pinsValues = 0U;
-    uint8_t inputMuxIterator;
+    uint32_t reg_val;
 
-        /* Get the SIUL2 base for interrupt pin if using */
-        //interruptBase = SIUL2_1;
+    do {
+        reg_val = (uart->linsr & LINFLEXD_LINSR_LINS_MASK) >> LINFLEXD_LINSR_LINS_SHIFT;
+    } while (reg_val == LINFLEXD_LINSR_LINS_DRDT || reg_val == LINFLEXD_LINSR_LINS_HRT);
 
-	pinsValues |= SIUL2_1_MSCR_OBE(config->outputBuffer);
-    pinsValues |= SIUL2_1_MSCR_ODE(config->openDrain);
-    pinsValues |= SIUL2_1_MSCR_IBE(config->inputBuffer);
-    pinsValues |= SIUL2_1_MSCR_SRE(config->slewRateCtrlSel);
-    pinsValues |= SIUL2_1_MSCR_RCVR(config->receiverSel);
-    pinsValues |= SIUL2_1_MSCR_SMC(config->safeMode);
-    pinsValues |= SIUL2_1_MSCR_SSS(config->mux);
+    uart->bdrl = (uint32_t)c;
 
-    /* Configure initial value for GPIO pin in GPIO mux */
-    if ((PORT_MUX_AS_GPIO == config->mux) && (PORT_OUTPUT_BUFFER_ENABLED == config->outputBuffer))
+}
+
+void linflexd_uart_puts(volatile struct linflexd * uart, const char *s)
+{
+    while (*s)
     {
-        PINS_WritePin((GPIO_Type *) config->base->MSCR[config->pinPortIdx], 
-                        (pins_channel_type_t)(config->pinPortIdx % SIUL2_NUM_OF_PIN_PORT), pinsValues);
-    }
-
-    /* Write to Multiplexed Signal Configuration Register register */
-    config->base->MSCR[config->pinPortIdx] = pinsValues;
-
-    // INPUT (Rx) Config Only
-    if (PORT_INPUT_BUFFER_ENABLED == config->inputBuffer)
-    {
-        for (inputMuxIterator = 0U; inputMuxIterator < FEATURE_SIUL2_INPUT_MUX_WIDTH; inputMuxIterator++)
-        {
-            /* Check if input mux information needs to be configured */
-            if (config->inputMux[inputMuxIterator] != PORT_INPUT_MUX_NO_INIT)
-            {
-                /* Write to Input Mux register */
-                config->base->IMCR[config->inputMuxReg[inputMuxIterator]] =
-                                SIUL2_AE_IMCR_SSS(config->inputMux[inputMuxIterator]);
-            }
-        }
-
-
-        //DEV_ASSERT(config->intConfig.eirqPinIdx < SIUL2_INTERRUPT_COUNT);
-        /* Get the appropriate value follow index of pin interrupt */
-        //pinIntValue = (1UL << config->intConfig.eirqPinIdx);
-        
-        /* Mask interrupts by clearing the EIREn bits in DIRER0 */
-        //interruptBase->DIRER0 &= ~pinIntValue;
+        linflexd_uart_putc(uart,*s++);
     }
 }
--- src/platform/s32z270/s32z270.c
+++ src/platform/s32z270/s32z270.c
@@ -1,61 +1,30 @@
-#include <psi5s_uart.h>
 #include <linflexd_uart.h>
 
 
-struct Psi5s_Uart *uart  = /*(LINFlexD_Type*)*/ (void*) 0x40170000;    //Linflexd0
-// struct Psi5s_Uart *uart  = /*(LINFlexD_Type*)*/ (void*) 0x42980000;    //Linflexd9
-
-struct LINFlexD{
-    uint32_t LINCR1;          /**< LIN Control Register 1, offset: 0x0 */
-    uint32_t LINIER;          /**< LIN Interrupt Enable Register, offset: 0x4 */
-    uint32_t LINSR;           /**< LIN Status Register, offset: 0x8 */
-    uint32_t LINESR;          /**< LIN Error Status Register, offset: 0xC */
-    uint32_t UARTCR;          /**< UART Mode Control Register, offset: 0x10 */
-    uint32_t UARTSR;          /**< UART Mode Status Register, offset: 0x14 */
-    uint32_t LINTCSR;         /**< LIN Time-Out Control Status Register, offset: 0x18 */
-    uint32_t LINOCR;          /**< LIN Output Compare Register, offset: 0x1C */
-    uint32_t LINTOCR;         /**< LIN Time-Out Control Register, offset: 0x20 */
-    uint32_t LINFBRR;         /**< LIN Fractional Baud Rate Register, offset: 0x24 */
-    uint32_t LINIBRR;         /**< LIN Integer Baud Rate Register, offset: 0x28 */
-    uint32_t LINCFR;          /**< LIN Checksum Field Register, offset: 0x2C */
-    uint32_t LINCR2;          /**< LIN Control Register 2, offset: 0x30 */
-    uint32_t BIDR;            /**< Buffer Identifier Register, offset: 0x34 */
-    uint32_t BDRL;            /**< Buffer Data Register Least Significant, offset: 0x38 */
-    uint32_t BDRM;            /**< Buffer Data Register Most Significant, offset: 0x3C */
-    uint8_t RESERVED_0[12];
-    uint32_t GCR;             /**< Global Control Register, offset: 0x4C */
-    uint32_t UARTPTO;         /**< UART Preset Timeout Register, offset: 0x50 */
-    uint32_t UARTCTO;         /**< UART Current Timeout Register, offset: 0x54 */
-    uint32_t DMATXE;          /**< DMA Tx Enable Register, offset: 0x58 */
-    uint32_t DMARXE;          /**< DMA Rx Enable Register, offset: 0x5C */
-};
+volatile struct linflexd *uart  =  (volatile struct linflexd *)PLAT_UART_ADDR;
 
 void uart_init(void)
 {
-    psi5_uart_init(uart);
-    psi5_uart_enable(uart);
-
-    return;
+    linflexd_uart_init(uart);
+    linflexd_uart_enable(uart);
 }
 
 void uart_putc(char c)
 {
-    psi5_uart_putc(uart, c);
+    linflexd_uart_putc(uart, c);
 }
 
 char uart_getchar(void)
 {
-    return psi5_uart_getc(uart);
+    return linflexd_uart_getc(uart);
 }
 
-void uart_enable_rxirq(){
-
+void uart_enable_rxirq(void)
+{
+    linflexd_uart_rxirq(uart);
 }
 
-void uart_clear_rxirq(){
-    // struct LINFlexD *temp = (void*) 0x42980000;
-    struct LINFlexD *temp = (void*) 0x40170000;
-    //while(!(temp->UARTSR & 0x200));
-    unsigned char data = temp->BDRM;
-    temp->UARTSR |= 4;
+void uart_clear_rxirq(void)
+{
+    linflexd_uart_clear_rxirq(uart);
 }
